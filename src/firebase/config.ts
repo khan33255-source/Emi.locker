986771ffe2b90791bd257e8a03c96584cb215098
F.K whatsapp status video song <khan33255@gmail.com>
2026-02-08 15:57:31 +0000

    I have the content of my google-services.json file. Can you create the f

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDWKhyIcz3wIk_rQQbc9HY8TRRVx8GsUs0",
+  authDomain: "emilocker-a9f98.firebaseapp.com",
+  projectId: "emilocker-a9f98",
+  storageBucket: "emilocker-a9f98.firebasestorage.app",
+  messagingSenderId: "1066870697843",
+  appId: "1:1066870697843:web:your_web_app_id_here" // Note: This should ideally be replaced with your actual web app ID from Firebase console
+};
